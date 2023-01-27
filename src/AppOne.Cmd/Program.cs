e1f085366295e02636d7b018e248b6ecb3493796
ronh-iq <ron.hodgson@iqcu.com>
2023-01-26 16:31:38 -0800

    cmd

--- src/AppOne.Cmd/Program.cs
+++ src/AppOne.Cmd/Program.cs
@@ -1,10 +1,24 @@
-﻿namespace AppOne.Cmd
+﻿using LibAlpha.PrjOne;
+using LibAlpha.PrjTwo;
+using LibBeta.PrjOne;
+using LibBeta.PrjTwo;
+
+namespace AppOne.Cmd
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var libAlphaPrjOneClass = new LibAlphaPrjOneClass();
+            var libAlphaPrjTwoClass = new LibAlphaPrjTwoClass();
+            var libBetaPrjOneClass = new LibBetaPrjOneClass();
+            var libBetaPrjTwoClass = new LibBetaPrjTwoClass();
+
+            Console.WriteLine(
+                libBetaPrjTwoClass.AppendName(
+                    libBetaPrjOneClass.AppendName(
+                        libAlphaPrjTwoClass.AppendName(
+                            libAlphaPrjOneClass.AppendName("AppOne.Cmd:")))));
         }
     }
 }
